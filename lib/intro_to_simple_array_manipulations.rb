48ea23189af189837520aca5e3316972bcfa653a
Allen Shin <geeuho@gmail.com>
2019-10-21 19:45:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,12 +2,12 @@ def using_push(array, element)
   array.push(element)
 end
 
-def using_unshift
-  
+def using_unshift(array, element)
+  array.unshift(element)
 end
 
 def using_pop
-
+  
 end
 
 def pop_with_args
