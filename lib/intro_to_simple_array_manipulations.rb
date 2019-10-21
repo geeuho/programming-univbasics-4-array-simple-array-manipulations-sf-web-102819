9440dfee05f1c30edb849836be618792027be33f
Allen Shin <geeuho@gmail.com>
2019-10-21 19:46:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,47 +1,47 @@
-def using_push(array, element)
+def using_push (array, element)
   array.push(element)
 end
 
-def using_unshift(array, element)
+def using_unshift (array, element)
   array.unshift(element)
 end
 
-def using_pop
+def using_pop (array, element)
   
 end
 
-def pop_with_args
+def pop_with_args (array, element)
   
 end
 
-def using_shift 
+def using_shift (array, element)
   
 end
 
-def shift_with_args
+def shift_with_args (array, element)
   
 end
 
-def using_concat
+def using_concat (array, element)
   
 end
 
-def using_insert
+def using_insert (array, element)
   
 end
 
-def using_uniq
+def using_uniq (array, element)
   
 end
 
-def using_flatten
+def using_flatten (array, element)
   
 end
 
-def using_delete
+def using_delete (array, element)
 
 end
 
-def using_delete_at
+def using_delete_at (array, element)
   
 end
